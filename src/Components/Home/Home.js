bf528d2a25b9fa5c58ed32a64d2af93cbdc0f554
petrakovpk <petrakovpk@gmail.com>
2020-02-26 12:01:12 +0300

    first

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+
+const Home = ({strings})=> (
+<div>
+
+<Container>
+    <Row>
+        <Col>
+            lol
+        </Col>
+    </Row>
+
+</Container>
+
+</div>
+
+
+)
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+const app = (
+
+      <App />
+
+);
+
+if (rootElement.hasChildNodes()) {
+  hydrate(app, rootElement);
+} else {
+  render(app, rootElement);
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
