e427578659eece926e7f9148a6eef12ec375b7e8
petrakovpk <petrakovpk@gmail.com>
2020-02-28 17:09:15 +0300

    5

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,8 +7,28 @@ class Home extends React.Component {
         super(props);
         this.state = {
             heroes : [],
+            contradict_prediction : {hero: [], val: []} ,
+            support_prediction_hero : [] ,
+            support_prediction_val : [],
             radiantHero1 : '',
-            direHero1 : ''
+            radiantHero1_val :0,
+            direHero1 : '',
+            radiantHero2 : '',
+            radiantHero2_val :0,
+            direHero2 : '',
+            radiantHero3 : '',
+            radiantHero3_val :0,
+            direHero3 : '',
+            radiantHero4 : '',
+            radiantHero4_val :0,
+            direHero4 : '',
+            radiantHero5 : '',
+            radiantHero5_val :0,
+            direHero5 : '',
+            confidence_false : 0,
+            confidence_true: 0,
+            radiantScore1: 0,
+            direScore1: 0,
         };
 
 
@@ -20,7 +40,8 @@ class Home extends React.Component {
             .then(res => res.json())
             .then((result) => {
                     for (var i = 0; i < result.length; i++) {
-                        this.state.heroes[i] = {key: result[i]['id'], value: result[i]['localized_name'], text: result[i]['localized_name']}
+                        this.state.heroes[i] = {key: result[i]['id'], value: result[i]['name'].substring(14, ), text: result[i]['localized_name']}
+
                     }
 
                 }
@@ -45,6 +66,12 @@ class Home extends React.Component {
 
     }
 
+     getIndex = (name) => {
+      return this.state.contradict_prediction.hero.findIndex( function(hero){
+          return hero === name
+      } );
+    }
+
 
     getMatchResult() {
 
@@ -52,12 +79,12 @@ class Home extends React.Component {
         const url = "https://go.rapidminer.com/am/api/deployments/6882a8a2-92c6-445c-932c-f5c2dd2a3d07"
 
         const post_r = JSON.stringify({
-                radiant_hero_1_name: 'lina',
+                radiant_hero_1_name: this.state.radiantHero1 ,
                 radiant_hero_2_name: 'lina',
                 radiant_hero_3_name: 'lina',
                 radiant_hero_4_name: 'lina',
                 radiant_hero_5_name: 'lina',
-                dire_hero_1_name: 'lina',
+                dire_hero_1_name: this.state.direHero1 ,
                 dire_hero_2_name: 'lina',
                 dire_hero_3_name: 'lina',
                 dire_hero_4_name: 'lina',
@@ -78,16 +105,74 @@ class Home extends React.Component {
             }
 
 
-        ).then(res => res.json()).then(data => console.log(data))
+        ).then(res => res.json()).then((data) => {
+
+                this.setState( {contradict_prediction : {hero: [], val: []}})
+
+
+                const contradict_len = data['data'][0]['Contradict Prediction'].split(';').length;
+                const contradict_arr = data['data'][0]['Contradict Prediction'].split(';');
+
+                for (var i = 0; i < contradict_len; i++) {
+
+                    const contradict_elem = contradict_arr[i].split("=").map(Function.prototype.call, String.prototype.trim)
+
+                    const concat_array = {
+                        hero: contradict_elem[0],
+                        val: parseFloat(contradict_elem[1].split("(")[1].substr(0, 6), 10)
+
+                    }
+
+
+                    this.state.contradict_prediction.hero = this.state.contradict_prediction.hero.concat(concat_array.hero)
+                    this.state.contradict_prediction.val = this.state.contradict_prediction.val.concat(concat_array.val)
+
+
+
+                }
+
+
+
+                console.log( this.state.contradict_prediction.hero )
+
+                console.log( this.state.contradict_prediction.val[this.getIndex("radiant_hero_4_name")])
 
 
 
 
+                const support_len = data['data'][0]['Support Prediction'].split(';').length;
+                const support_arr = data['data'][0]['Support Prediction'].split(';')
+
+                for (var i = 0; i < support_len; i++) {
+
+                    const support_elem = support_arr[i].split("=").map(Function.prototype.call, String.prototype.trim)
+
+                    this.setState({support_prediction_hero: this.state.support_prediction_hero.concat(support_elem[0])})
+
+                    this.setState({
+                        support_prediction_val: this.state.support_prediction_val.concat(
+                            parseFloat(support_elem[1].split("(")[1].substr(0, 6), 10)
+                        )
+                    })
+                }
 
 
-    }
 
 
+            this.state.confidence_false = Math.round(data['data'][0]['confidence(False)']*100);
+            this.state.confidence_true = Math.round(data['data'][0]['confidence(True)']*100);
+
+
+            return data
+
+            }
+
+
+
+             )
+
+
+        }
 
 
     render() {
@@ -95,6 +180,22 @@ class Home extends React.Component {
         return (
 
         <div className="container-fluid">
+
+            <div className="row my-auto">
+
+                 <div className="col-lg-1 offset-lg-5">
+
+                     {this.state.confidence_true}
+
+                </div>
+
+                 <div className="col-lg-3 ">
+
+                     {this.state.confidence_false}
+
+                </div>
+
+            </div>
 
             <div className="row my-auto">
 
@@ -106,13 +207,13 @@ class Home extends React.Component {
 
                 <div className="col-lg-1">
 
-                    <p>  </p>
+                    <p> {this.state.radiantHero1_val} </p>
 
                 </div>
 
                 <div className="col-lg-1">
 
-                     <p>  </p>
+                     <p> 2 </p>
 
                 </div>
 
@@ -122,6 +223,109 @@ class Home extends React.Component {
 
                 </div>
 
+            </div>
+
+            <div className="row my-auto">
+
+                <div className="col-lg-2 offset-lg-3">
+
+
+
+                </div>
+
+                <div className="col-lg-1">
+
+                    <p> {this.state.radiantHero2_val} </p>
+
+                </div>
+
+                <div className="col-lg-1">
+
+                     <p> 2 </p>
+
+                </div>
+
+                <div className="col-lg-2">
+
+                </div>
+
+            </div>
+
+             <div className="row my-auto">
+
+                <div className="col-lg-2 offset-lg-3">
+
+
+
+                </div>
+
+                <div className="col-lg-1">
+
+                    <p> {this.state.radiantHero3_val} </p>
+
+                </div>
+
+                <div className="col-lg-1">
+
+                     <p> 2 </p>
+
+                </div>
+
+                <div className="col-lg-2">
+
+                </div>
+
+            </div>
+
+             <div className="row my-auto">
+
+                <div className="col-lg-2 offset-lg-3">
+
+
+
+                </div>
+
+                <div className="col-lg-1">
+
+                    <p> {this.state.radiantHero4_val} </p>
+
+                </div>
+
+                <div className="col-lg-1">
+
+                     <p> 2 </p>
+
+                </div>
+
+                <div className="col-lg-2">
+
+                </div>
+
+            </div>
+
+             <div className="row my-auto">
+
+                <div className="col-lg-2 offset-lg-3">
+
+
+
+                </div>
+
+                <div className="col-lg-1">
+
+                    <p> {this.state.radiantHero5_val} </p>
+
+                </div>
+
+                <div className="col-lg-1">
+
+                     <p> 2 </p>
+
+                </div>
+
+                <div className="col-lg-2">
+
+                </div>
 
             </div>
 
